0f0d6d649e5726daf4e45b21dc10f1761d629e1e
C9BrenoFR <breno.rosado@estudante.ufjf.br>
2025-06-04 21:18:48 -0300

    refactor: simplify HomeScreen component by removing unused render logic

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,9 @@
-import FindPokemon from "@/components/FindPokemon";
-import HomeSquare from "@/components/HomeSquare";
-import UfjfMenu from "@/components/UfjfMenu";
-
-const getCurrentDate = (): string => {
-  const today = new Date();
-  const day = String(today.getDate()).padStart(2, '0');
-  const month = String(today.getMonth() + 1).padStart(2, '0'); // Os meses começam do 0
-  const year = today.getFullYear();
-  return `${day}/${month}/${year}`;
-};
-
+import { Text, View } from "react-native";
 
 export default function HomeScreen() {
-  function render(atv: number) {
-    if (atv === 1)
-      return (<UfjfMenu date={getCurrentDate()} />)
-    if (atv === 2)
-      return (<HomeSquare />)
-    if (atv === 3)
-      return (<FindPokemon />)
-
-  }
-
-  return render(3);
+  return (
+    <View>
+      <Text>Bem vindo a Home, selecione uma modalidade</Text>
+    </View>
+  );
 }
